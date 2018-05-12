1d317c6a103b22b3c98d0a79fbbe42ed7377da66
ckeokot <ckeokot@gmail.com>
2018-05-11 17:12:16 -0700

    added command to check author id of message

--- extraCog.py
+++ extraCog.py
@@ -50,6 +50,10 @@ class extraCog:
         output = self.bot.data[ctx.message.server.id]
         await self.bot.say(output)
 
+    @commands.command(pass_context=True):
+    async def id(self, ctx):
+        await self.bot.say(ctx.message.author.id)
+
 
 
 def setup(bot):

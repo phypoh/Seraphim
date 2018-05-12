8e8a83d6b39590c8e8ea69fea5f61824bf284ecf
ckeokot <ckeokot@gmail.com>
2018-05-11 18:32:58 -0700

    added seperate user drafts through the use of dictionaries
    multiple users can draft at the same time now

--- main.py
+++ main.py
@@ -19,11 +19,14 @@ bot = commands.Bot(command_prefix='!')
 bot.API_rates = pull_all()
 
 
-bot.A_ban = []
-bot.B_ban = []
-bot.A_side = []
-bot.B_side = []
-bot.side = 1
+#bot.A_ban = []
+#bot.B_ban = []
+#bot.A_side = []
+#bot.B_side = []
+#bot.side = 1
+
+bot.ids = set()
+bot.draft_dict = {}
 
 @bot.event
 async def on_ready():
@@ -70,143 +73,170 @@ async def reload(cog):
 #==============================================================================
 # Main Draft UI
 #==============================================================================
-@bot.command()
-async def start():
+@bot.command(pass_context=True)
+async def start(ctx):
     """
     Starts the draft. Competitive 5v5 format
     """
-    
+    # Clears the draft data since you are starting a new draft
+    data = id_to_dict_clear(ctx.message.author.id)
+
     await bot.say("Draft format: (Ban) ABAB (Pick) ABBAABBAAB")
-    if bot.side == 1:
+    if data["side"] == 1:
         await bot.say("Player side: A")
-    if bot.side == 0:
+    if data["side"] == 0:
         await bot.say("Player side: B")
     
-    bot.A_ban = []
-    bot.B_ban = []
-    bot.A_side = []
-    bot.B_side = []
-    
-    bot.AI_turn = bot.side
-    turn_time()
-    output = turn_check()
+    bot.AI_turn = data["side"]
+    data = turn_time(data)
+    data, output = turn_check(data)
     await bot.say(output)
 
-def turn_check():
+def id_to_dict(id):
+    """
+    Helper function to access draft data based on user id.
+    """
+    # If id isn't in draft_dict, set it up.
+    if id not in bot.ids:
+        bot.ids.add(id)
+        return id_to_dict_clear(id)
+    else:
+        return bot.draft_dict[id]
+
+def id_to_dict_clear(id):
+    """
+    Clears the values of the dict based on id.
+    """
+    bot.draft_dict[id] = {
+        "A_ban": [],
+        "B_ban": [],
+        "A_side": [],
+        "B_side": [],
+        "side": 1,
+        "AI_turn": 1
+    }
+    return bot.draft_dict[id]
+
+def turn_check(data): #TODO
     output = ""
 
-    if len(bot.B_ban) < 2:
-        if bot.AI_turn == 0:
+    if len(data["B_ban"]) < 2:
+        if data["AI_turn"] == 0:
             output += "Ban a hero"
             return output
-        elif bot.AI_turn == 1:
-            chosen_hero = AI_ban(bot.A_side, bot.B_side, bot.A_ban, bot.B_ban)
-            if bot.side == 0:
-                bot.A_ban.append(chosen_hero)
+        elif data["AI_turn"] == 1:
+            chosen_hero = AI_ban(data["A_side"], data["B_side"], data["A_ban"], data["B_ban"])
+            if data["side"] == 0:
+                data["A_ban"].append(chosen_hero)
                 output += "AI has banned " + chosen_hero + "\n"
-                turn_time()
-                output += turn_check()
-            elif bot.side == 1:
-                bot.B_ban.append(chosen_hero)
+                data = turn_time(data)
+                output += turn_check(data)
+            elif data["side"] == 1:
+                data["B_ban"].append(chosen_hero)
                 output += "AI has banned " + chosen_hero + "\n"
-                turn_time()
-                output += turn_check()
+                data = turn_time(data)
+                output += turn_check(data)
                 
-    elif len(bot.B_side) < 5:
-        if bot.AI_turn == 0:
+    elif len(data["B_side"]) < 5:
+        if data["AI_turn"] == 0:
             output += "Pick a hero"
-            return output
-        elif bot.AI_turn == 1:
-            chosen_hero = AI_pick(bot.A_side, bot.B_side, bot.A_ban, bot.B_ban)
-            if bot.side == 0:
-                bot.A_side.append(chosen_hero)
+            return data, output
+        elif data["AI_turn"] == 1:
+            chosen_hero = AI_pick(data["A_side"], data["B_side"], data["A_ban"], data["B_ban"])
+            if data["side"] == 0:
+                data["A_side"].append(chosen_hero)
                 output += "AI has selected " + chosen_hero + "\n"
-                turn_time()
-                output += turn_check()
-            elif bot.side == 1:
-                bot.B_side.append(chosen_hero)
+                data = turn_time(data)
+                output += turn_check(data)
+            elif data["side"] == 1:
+                data["B_side"].append(chosen_hero)
                 output += "AI has selected " + chosen_hero + "\n"
-                turn_time()
-                output += turn_check()
-    elif len(bot.B_side) == 5:
+                data = turn_time(data)
+                output += turn_check(data)
+    elif len(data["B_side"]) == 5:
         output += print_log(bot)        
-    return output
+    return data, output
      
-def turn_time():
-    if len(bot.B_ban) < 2:
-        bot.AI_turn = (bot.AI_turn+1)%2 
+def turn_time(data):
+    if len(data["B_ban"]) < 2:
+        data["AI_turn"] = (data["AI_turn"]+1)%2
         
-    elif len(bot.B_side) < 5:
+    elif len(data["B_side"]) < 5:
         #If it's A's turn now
-        if (len(bot.A_side)%2 == 0 or (len(bot.A_side)%2 == 1 and len(bot.B_side) > len(bot.A_side))):
-            if bot.side == 0:
-                bot.AI_turn = 1
+        if (len(data["A_side"])%2 == 0 or (len(data["A_side"])%2 == 1 and len(data["B_side"]) > len(data["A_side"]))):
+            if data["side"] == 0:
+                data["AI_turn"] = 1
             else:
-                bot.AI_turn = 0
+                data["AI_turn"] = 0
                 
         #If it's B's turn now
         else:
-            if bot.side == 1:
-                bot.AI_turn = 1
+            if data["side"] == 1:
+                data["AI_turn"] = 1
             else:
-                bot.AI_turn = 0
+                data["AI_turn"] = 0
     else:
-        bot.AI_turn = 2
+        data["AI_turn"] = 2
+    return data
         
     
 
-@bot.command()
-async def pick(hero):
+@bot.command(pass_context=True)
+async def pick(ctx, *, hero):
     """
     Picks a hero
     """
+    data = id_to_dict(ctx.message.author.id)
+
     hero = hero.title()
-    if bot.AI_turn == 1:
+    if data["AI_turn"] == 1:
         await bot.say("It's not your turn yet. Can't you be a little patient?")
-    elif len(bot.B_ban) < 2:
+    elif len(data["B_ban"]) < 2:
         await bot.say("Stop cheating. It's ban phase.")
-    elif (hero in bot.A_side or hero in bot.B_side):
-        await bot.say("Hero has alreay been picked. Pick another hero.")
-    elif (hero in bot.A_ban or hero in bot.B_ban):
+    elif (hero in data["A_side"] or hero in data["B_side"]):
+        await bot.say("Hero has already been picked. Pick another hero.")
+    elif (hero in data["A_ban"] or hero in data["B_ban"]):
         await bot.say("Hero has already been banned. Pick another hero.")
     elif (hero not in hero_list):
         await bot.say("Hero does not exist. Pick again.")
     else:
-        if bot.side == 1:
-            bot.A_side.append(hero)
-            turn_time()
+        if data["side"] == 1:
+            data["A_side"].append(hero)
+            data = turn_time(data)
             await bot.say("Player has selected " + hero)
         elif bot.side == 0:
-            bot.B_side.append(hero)
-            turn_time()
+            data["B_side"].append(hero)
+            data = turn_time(data)
             await bot.say("Player has selected " + hero)
-        output = turn_check()
+        data, output = turn_check(data)
         await bot.say(output)
         
-@bot.command()
-async def ban(hero):
+@bot.command(pass_context=True)
+async def ban(ctx, *, hero):
     """
     Bans a hero
     """
+    data = id_to_dict(ctx.message.author.id)
+
     hero = hero.title()
-    if len(bot.B_ban) >= 2:
+    if len(data["B_ban"]) >= 2:
         await bot.say("Stop cheating. You're only supposed to ban a maximum of 2 heroes.")
-    elif bot.AI_turn == 1:
+    elif data["AI_turn"] == 1:
         await bot.say("It's not your turn yet. Can't you be a little patient?")
-    elif (hero in bot.A_ban or hero in bot.B_ban):
+    elif (hero in data["A_ban"] or hero in data["B_ban"]):
         await bot.say("Hero has already been banned. Ban another hero.")
     elif (hero not in hero_list):
         await bot.say("Hero does not exist. Ban again.")
     else:
-        if bot.side == 1:
-            bot.A_ban.append(hero)
-            turn_time()
+        if data["side"] == 1:
+            data["A_ban"].append(hero)
+            data = turn_time(data)
             await bot.say("Player has banned " + hero)
         elif bot.side == 0:
-            bot.B_ban.append(hero)
-            turn_time()
+            data["B_ban"].append(hero)
+            data = turn_time(data)
             await bot.say("Player has banned " + hero)
-        output = turn_check()
+        data, output = turn_check(data)
         await bot.say(output)
 
 bot.run(os.getenv('BOT_TOKEN'))
